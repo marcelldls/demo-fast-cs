84cc10861d218a4e892126daeefebcdf16773315
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2023-06-01 22:32:34 +0100

    Include reference to Controller in Mapping output

--- src/demo_fast_cs/fast_cs/controller.py
+++ src/demo_fast_cs/fast_cs/controller.py
@@ -1,28 +1,15 @@
 from __future__ import annotations
 
-from .api_methods import APIMethod
-from .mapping import Mapping
-
 
 class Controller:
     def __init__(self) -> None:
         self.__sub_controllers: list[SubController] = []
 
-    def register(self, controller: SubController):
+    def register_sub_controller(self, controller: SubController):
         self.__sub_controllers.append(controller)
 
-    @staticmethod
-    def get_api_methods(controller):
-        methods = [getattr(controller, method_name) for method_name in dir(controller)]
-        return [method for method in methods if isinstance(method, APIMethod)]
-
-    def get_mapping(self) -> Mapping:
-        # TODO: Ensure all methods are stored alongside relevant object
-        api_methods = []
-        for controller in [self, *self.__sub_controllers]:
-            api_methods += self.get_api_methods(controller)
-
-        return Mapping(api_methods)
+    def get_sub_controllers(self) -> list[SubController]:
+        return self.__sub_controllers
 
 
 class SubController:
--- src/demo_fast_cs/fast_cs/mapping.py
+++ src/demo_fast_cs/fast_cs/mapping.py
@@ -1,9 +1,44 @@
+from dataclasses import dataclass
+
 from .api_methods import APIMethod
+from .controller import Controller, SubController
+
+
+@dataclass
+class SingleMapping:
+    controller: Controller | SubController
+    methods: list[APIMethod]
 
 
 class Mapping:
-    def __init__(self, methods: list[APIMethod]):
-        self._methods = methods
+    def __init__(self, controller: Controller):
+        self._generate_mapping(controller)
 
-    def get_methods(self):
-        return self._methods
+    @staticmethod
+    def get_api_methods(controller: Controller | SubController):
+        methods = [getattr(controller, method_name) for method_name in dir(controller)]
+        return [method for method in methods if isinstance(method, APIMethod)]
+
+    def _generate_mapping(self, controller: Controller):
+        self._controller_mapping = SingleMapping(
+            controller, self.get_api_methods(controller)
+        )
+
+        self._sub_controller_mappings: list[SingleMapping] = []
+        for sub_controller in controller.get_sub_controllers():
+            self._sub_controller_mappings.append(
+                SingleMapping(sub_controller, self.get_api_methods(sub_controller))
+            )
+
+    def __str__(self):
+        result = f"Top-level mapping: {self._controller_mapping}\n"
+        result += "Sub-controller mappings:\n"
+        for sub_mapping in self._sub_controller_mappings:
+            result += f"{sub_mapping}\n"
+        return result
+
+    def get_controller_mapping(self):
+        return self._controller_mapping
+
+    def get_sub_controller_mappings(self):
+        return self._sub_controller_mappings
